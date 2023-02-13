bc2c5ec42517838069bc72cc10616c0e31c7ed7d
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-02-13 18:28:54 -0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,12 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { bagCheckOutline, cartOutline,    fileTrayStackedOutline,   home,  } from 'ionicons/icons';
+import { bagCheckOutline, cartOutline,    fileTrayStackedOutline,   globeOutline,   home,  } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 import Tab4 from './pages/Tab4';
+import Tab5 from './pages/Tab5';
 
 
 /* Core CSS required for Ionic components to work properly */
@@ -56,6 +57,9 @@ const App: React.FC = () => (
           <Route path="/tab4">
             <Tab4 />
           </Route>
+          <Route path="/tab5">
+            <Tab5 />
+          </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
@@ -74,7 +78,12 @@ const App: React.FC = () => (
             <IonLabel>Ventas</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab4" href="/tab4">
-            <IonIcon icon={fileTrayStackedOutline} />            <IonLabel>Inventario</IonLabel>
+            <IonIcon icon={fileTrayStackedOutline} />            
+            <IonLabel>Inventario</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab5" href="/tab5">
+            <IonIcon icon={globeOutline} />            
+            <IonLabel>Web</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
---
+++ src/pages/Tab5.tsx
@@ -0,0 +1,26 @@
+import {  IonContent, IonHeader, IonPage } from '@ionic/react';
+import './Tab4.css';
+import Toolbar from '../components/Toolbar';
+import Iframe from 'react-iframe'
+const Tab5: React.FC = () => {
+    return (
+        <IonPage>
+        <IonHeader>
+            <Toolbar/>
+        </IonHeader>
+        <IonContent fullscreen >
+        <Iframe url="https://www.appsheet.com/start/33e88794-aa19-4bc4-8699-c2824835688d"
+                width="100%"
+                height="100%"
+                id=""
+                allowFullScreen 
+                styles={{border: "0px"}}
+                className=""
+                display="block"
+                position="relative"/>
+        </IonContent>
+        </IonPage>
+    );
+};
+
+export default Tab5;
